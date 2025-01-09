cd039507cb75bfc660ef8ce42e2352f20bcb281b
santilaguado <santiagolaguado57@gmail.com>
2025-01-09 17:36:07 -0500

    fix: :ambulance: Arreglado Bug en Vite.COnfig

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from "path"
-import API_BASE_URL from './src/services/apiConfig';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
@@ -12,7 +11,11 @@ export default defineConfig({
   },
   server: {
     proxy: {
-      '/api': process.env.VITE_API_BASE_URL, 
+      '/api': {
+        target: process.env.VITE_API_BASE_URL || 'http://localhost:3000', // Usa un valor predeterminado si no está definido
+        changeOrigin: true,
+        secure: false,
+      },
     },
   },
 });
