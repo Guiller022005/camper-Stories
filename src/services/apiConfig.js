66ca030512b153c5f7d343f91d6677e5be93779a
Guiller022005 <118319910+Guiller022005@users.noreply.github.com>
2025-01-13 13:54:56 -0600

    feat: :sparkles: consumo de api, data renderizada lista

--- src/services/apiConfig.js
+++ src/services/apiConfig.js
@@ -4,12 +4,15 @@ export const endpoints = {
   login: `${API_BASE_URL}users/login`, // Endpoint para inicio de sesión
   register: `${API_BASE_URL}users/register`, // Endpoint para crear usuarios
   campers: `${API_BASE_URL}campers`, // Endpoint para crear sueno
+  egresados: `${API_BASE_URL}campers/graduates`, // Endpoint para obtener campers egresados
+  formados: `${API_BASE_URL}campers/trainees`, // Endpoint para obtener campers en proceso de formacion
   merits: `${API_BASE_URL}merits`,
   dreams: `${API_BASE_URL}campers/{id}/dreams`,
   tiktoks: `${API_BASE_URL}campers/{id}/videos`,
   city: `${API_BASE_URL}cities`, // Endpoint para crear sueno
   technology: `${API_BASE_URL}technology`,
   projects: `${API_BASE_URL}campers/{id}/proyects`,
+  merits: `${API_BASE_URL}merits/{id}`, // Endpoint para obtener los meritos de un usuario
   technologyProject: `${API_BASE_URL}projects/technologies`
 };
 
