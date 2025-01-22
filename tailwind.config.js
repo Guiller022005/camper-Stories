d8f0224815d4e83fde71f6d8a484ad8009101d4f
Andolon-M <aemendez-l@outlook.com>
2025-01-22 12:31:42 -0500

    feat: :lipstick: se sigue trabajando en la unificacion de los colores css mediante variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -21,7 +21,18 @@ module.exports = {
 				sm: 'calc(var(--radius) - 4px)',
 			},
 			colors: {
-				
+				color1: 'var(--color1)', // Color principal (blanco)
+				color2: 'var(--color2)', // Color secundario (amarillo/anaranjado)
+				color2hover: 'var(--color2-hover)',
+				color3: 'var(--color3)', // Color terciario (fondo oscuro)
+				color4: 'var(--color4)', // Color azul base
+				'color4-hover': 'var(--color4-hover)', // Hover del azul
+				'main-background-color': 'var(--main-background-color)', // Fondo principal
+				'main-text-color': 'var(--main-text-color)', // Color del texto principal
+
+				// Swiper Bullets
+				'swiper-bullet-bg': 'var(--swiper-bullet-bg)', // Color base de las balas
+				'swiper-bullet-hover-bg': 'var(--swiper-bullet-hover-bg)', // Color al pasar el mouse
 			},
 			fontFamily: {
 				firma: ['firma', 'sans-serif'], // Agrega la fuente personalizada.
