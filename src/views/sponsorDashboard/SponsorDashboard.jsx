4d568ffc9d73d694f7560a4ec918b9e1f8c15eb0
himayrobsarr <serganimon@gmail.com>
2025-01-28 16:11:49 -0500

    feat: :construction: "todo esta en construccion"

--- src/views/sponsorDashboard/SponsorDashboard.jsx
+++ src/views/sponsorDashboard/SponsorDashboard.jsx
@@ -1,3 +1,24 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 
 const NavbarProfile = lazy(() => import("../../components/navbar/NavbarProfile"));
+const Footer = lazy(() => import('../../components/footer/Footer'));
+
+const SponsorDashboard = () => {
+    // Aquí puedes definir el estado y la lógica que necesites
+
+    return (
+        <div className="sponsorDashboardView flex flex-col relative">
+            <Suspense fallback={<div>Cargando...</div>}>
+                <NavbarProfile />
+            </Suspense>
+            <div className="mainContent flex flex-col gap-4">
+                <p>hola , soy paco , como estas?</p>
+            </div>
+            <Suspense fallback={<div>Cargando...</div>}>
+                <Footer />
+            </Suspense>
+        </div>
+    );
+};
+
+export default SponsorDashboard;
