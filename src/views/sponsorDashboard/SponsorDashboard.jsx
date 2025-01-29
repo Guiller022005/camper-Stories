d61c7a6558c009b6a91bdd54561042d2b9833b07
himayrobsarr <serganimon@gmail.com>
2025-01-29 09:35:31 -0500

    feat: :construction: "seguimos haceindo cosas una detra de otra"

--- src/views/sponsorDashboard/SponsorDashboard.jsx
+++ src/views/sponsorDashboard/SponsorDashboard.jsx
@@ -1,24 +1,46 @@
-import React, { lazy, Suspense } from 'react';
+import React, { lazy, Suspense, useState, useEffect } from 'react';
+import Campers from "../../components/campersMainPage/Campers";
+import { fetchCamperById } from '../../services/camperService';  // Asegúrate de tener la ruta correcta
 
-const NavbarProfile = lazy(() => import("../../components/navbar/NavbarProfile"));
+const NavbarProfile = lazy(() => import("../../components/navbar/Navbar"));
 const Footer = lazy(() => import('../../components/footer/Footer'));
+const AboutMe = lazy(() => import('../../components/camperProfile/AboutMe'));
 
 const SponsorDashboard = () => {
-    // Aquí puedes definir el estado y la lógica que necesites
+  const [camperData, setCamperData] = useState({
+    main_video_url: '',
+    about: ''
+  });
 
-    return (
-        <div className="sponsorDashboardView flex flex-col relative">
-            <Suspense fallback={<div>Cargando...</div>}>
-                <NavbarProfile />
-            </Suspense>
-            <div className="mainContent flex flex-col gap-4">
-                <p>hola , soy paco , como estas?</p>
-            </div>
-            <Suspense fallback={<div>Cargando...</div>}>
-                <Footer />
-            </Suspense>
-        </div>
-    );
+  useEffect(() => {
+    const loadData = async () => {
+      try {
+        // Aquí debes especificar el ID del camper que quieres mostrar
+        const data = await fetchCamperById(1); // Por ejemplo, camper con ID 1
+        setCamperData(data);
+      } catch (error) {
+        console.error("Error loading camper data:", error);
+      }
+    };
+
+    loadData();
+  }, []);
+
+  return (
+    <div className="sponsorDashboardView flex flex-col relative">
+      <NavbarProfile />
+      <div className="mainContent flex flex-col gap-4">
+        <AboutMe 
+          videoUrl={camperData.main_video_url}
+          about={camperData.about}
+        />
+        <Campers />
+      </div>
+      <Suspense fallback={<div>Cargando...</div>}>
+        <Footer />
+      </Suspense>
+    </div>
+  );
 };
 
 export default SponsorDashboard;
