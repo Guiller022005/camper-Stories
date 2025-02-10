3144f496ecb2c4356813417e136c4ec350015625
himayrobsarr <serganimon@gmail.com>
2025-02-10 09:40:08 -0500

    feat: :sparkles: "sponsors en proceso"

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -60,7 +60,7 @@ const AppRouter = () => {
         <Route path='/terminos-y-condiciones' element={<TermsAndConditions/>}/>
 
         <Route 
-          path="/sponsor" 
+          path="/sponsor/:id" 
           element={
             <ProtectedRoute allowedRoles={['admin', 'sponsor']}>
               <Sponsors />
