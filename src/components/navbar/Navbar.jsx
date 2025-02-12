5844f0b4ffa87eee1a60c7cc1f7e29e1e32c3967
JhonJMD <jjmdcampus@gmail.com>
2025-02-12 07:42:42 -0500

    fix: :bug: Se arregla funcionalidades en vista movil y tablet editar y ver perfil

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -5,7 +5,7 @@ import campusLogo from "../../assets/campus.svg";
 import campusLogoCompleto from "../../assets/CampusLogo.png";
 import { toast } from "react-toastify";
 import { useCampus } from "../campersMainPage/context/CampusContext";
-import { ChevronDown, ChevronRight, ChevronUp } from "lucide-react";
+import { ChevronDown, ChevronUp } from "lucide-react";
 
 const Navbar = ({ viewType, links, onLinkClick }) => {
   const navigate = useNavigate();
@@ -19,39 +19,32 @@ const Navbar = ({ viewType, links, onLinkClick }) => {
   window.CampusState = currentCampusId;
 
   const campus = [
-    {
-      id: 1,
-      name: 'Bucaramanga'
-    },
-    {
-      id: 2,
-      name: 'Bogotá'
-    },
-    {
-      id: 3,
-      name: 'Tibú'
-    }
+    { id: 1, name: "Bucaramanga" },
+    { id: 2, name: "Bogotá" },
+    { id: 3, name: "Tibú" },
   ];
 
   const handleCampusClick = (campusId) => {
     console.log("🔄 Cambiando campus a:", campusId);
-    updateCampus(campusId); // Usa el método del contexto en lugar de `setState`
-    if (currentCampusId === campusId) {
-      return;
-    }
-    const selectedCampus = campus.find((c) => c.id === campusId); // Busca el campus por ID metodo find
-    const campusName = selectedCampus ? selectedCampus.name : "Campus desconocido"; // Guarda el nombre del campus o deja un mensaje en default
-    toast.info(`Campus seleccionado: ${campusName}`)
+    updateCampus(campusId);
+    if (currentCampusId === campusId) return;
+    const selectedCampus = campus.find((c) => c.id === campusId);
+    const campusName = selectedCampus ? selectedCampus.name : "Campus desconocido";
+    toast.info(`Campus seleccionado: ${campusName}`);
   };
 
   const toggleDropdown = () => {
     setIsDropdownOpen(!isDropdownOpen);
   };
 
-
   useEffect(() => {
     const token = localStorage.getItem("token");
     setIsLoggedIn(!!token);
+
+    return () => {
+      // Limpieza: restablece el scroll al desmontar el componente
+      document.body.style.overflow = "";
+    };
   }, [location.pathname]);
 
   const handleToggleProfile = () => {
@@ -61,19 +54,23 @@ const Navbar = ({ viewType, links, onLinkClick }) => {
         : `/campers/profile/${camperIdFromStorage}/edit`
     );
     setIsMenuOpen(false);
-    document.body.style.overflow = "";
+    document.body.style.overflow = ""; // Restablece el scroll
+  };
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+    document.body.style.overflow = isMenuOpen ? "" : "hidden"; // Controla el scroll
   };
 
   const handleLinkClick = (link) => {
     if (onLinkClick && link.href.startsWith("#")) {
-      onLinkClick(link.href.substring(1)); // Remueve '#' y hace scroll
+      onLinkClick(link.href.substring(1));
     } else {
-      navigate(link.href); // Navegar si es una URL normal
+      navigate(link.href);
     }
     setIsMenuOpen(false);
-    document.body.style.overflow = "";
+    document.body.style.overflow = ""; // Restablece el scroll
   };
-
 
   const handleLogout = () => {
     const logoutUrl = `${import.meta.env.VITE_API_BASE_URL}users/logout`;
@@ -100,71 +97,19 @@ const Navbar = ({ viewType, links, onLinkClick }) => {
       });
   };
 
-  const isLanding =
-    viewType === "landing" &&
-    location.pathname !== "/politica-de-privacidad" &&
-    location.pathname !== "/terminos-y-condiciones";
-
-  const navbarStyles = isLanding
-    ? `text-white p-3 top-0 z-20 transition-transform duration-300 shadow-lg border-b bg-[#27247a] border-indigo-700/30 backdrop-filter ${isMenuOpen ? "fixed bg-[#27247a]" : "bg-[#27247a] backdrop-blur-sm"
-    } w-full`
-    : `text-white p-3 fixed top-0 left-0 right-0 z-[1000] w-full font-inter ${isMenuOpen ? "bg-[#070727]" : "bg-[#070727]/20 backdrop-blur-md"
+  const navbarStyles = `text-white p-3 fixed top-0 left-0 right-0 z-[1000] w-full font-inter ${isMenuOpen ? "bg-[#070727]" : "bg-[#070727]/20 backdrop-blur-md"
     } shadow-lg transition-colors duration-300 ease-in-out`;
 
-  const toggleMenu = () => {
-    setIsMenuOpen(!isMenuOpen);
-    document.body.style.overflow = isMenuOpen ? "" : "hidden";
-  };
-
   const NavLinks = ({ links }) =>
-    links.map((link) => {
-      if (link.label === "Sedes") {
-        return (
-          <div key={link.label} className="relative hidden lg:block">
-            <button
-              onClick={toggleDropdown}
-              className="text-white text-lg py-3 hover:text-blue-400 transition flex items-center"
-            >
-              {link.label}
-              {isDropdownOpen ? (
-                <ChevronDown className="mt-1 h-5 w-full"/>
-              ) : (
-                <ChevronUp className="mt-1 h-5 w-full"/>
-              )}
-            </button>
-            {isDropdownOpen && ( 
-              <div className="absolute top-10 left-0 bg-indigo-500 text-black shadow-md rounded-lg w-48">
-                {campus.map((campusItem) => (
-                  <button
-                    key={campusItem.id}
-                    onClick={() => {
-                      handleCampusClick(campusItem.id);
-                      setIsDropdownOpen(false); // Cerrar dropdown
-                    }}
-                    className={`block w-full text-left px-4 py-2 hover:bg-indigo-500 hover:text-white ${currentCampusId === campusItem.id
-                        ? "bg-indigo-500 text-white"
-                        : "bg-[#27247a] text-white"
-                      }`}
-                  >
-                    {campusItem.name}
-                  </button>
-                ))}
-              </div>
-            )}
-          </div>
-        );
-      }
-      return (
-        <button
-          key={link.href || link.id}
-          onClick={() => handleLinkClick(link)}
-          className="text-white text-lg py-3 hover:text-blue-400 transition"
-        >
-          {link.label}
-        </button>
-      );
-    });
-
+    links.map((link) => (
+      <button
+        key={link.href || link.id}
+        onClick={() => handleLinkClick(link)}
+        className="text-white text-lg py-3 hover:text-blue-400 transition"
+      >
+        {link.label}
+      </button>
+    ));
 
   return (
     <nav className={navbarStyles}>
@@ -189,21 +134,13 @@ const Navbar = ({ viewType, links, onLinkClick }) => {
                 >
                   Ver Perfil
                 </Button>
-              ) : location.pathname.startsWith(`/campers/profile/${camperIdFromStorage}`) ? (
+              ) : (
                 <Button
                   onClick={() => navigate(`/campers/profile/${camperIdFromStorage}/edit`)}
                   size="lg"
                   className="text-lg bg-transparent hover:bg-[#4c47b4]"
                 >
                   Editar Perfil
-                </Button>
-              ) : (
-                <Button
-                  onClick={() => navigate(`/campers/profile/${camperIdFromStorage}`)}
-                  size="lg"
-                  className="text-lg bg-transparent hover:bg-[#4c47b4]"
-                >
-                  Ver Perfil
                 </Button>
               )}
               <Button
@@ -258,20 +195,34 @@ const Navbar = ({ viewType, links, onLinkClick }) => {
         <div className="fixed inset-0 bg-[#0C0C74] flex flex-col justify-center items-center z-[999]">
           <NavLinks links={links} />
           {!isLoggedIn ? (
-            <div className="flex flex-col gap-4"> {/* Agrega un gap de 4 unidades (1rem) entre los botones */}
+            <div className="flex flex-col gap-4">
               <Button size="lg" className="text-lg bg-transparent hover:bg-[#4c47b4]" onClick={() => navigate("/register")}>
-                Registrate
+                Regístrate
               </Button>
               <Button size="lg" className="text-lg bg-[#4c47b4] hover:bg-[#615cc2]" onClick={() => navigate("/login")}>
                 Inicia Sesión
               </Button>
             </div>
           ) : (
-            <div className="flex flex-col gap-4"> {/* Agrega un gap de 4 unidades (1rem) entre los botones */}
-              <Button size="lg" className="text-lg bg-transparent" onClick={handleToggleProfile}>
-                Ver Perfil
-              </Button>
-              <Button size="lg" className="text-lg bg-red-500" onClick={handleLogout}>
+            <div className="flex flex-col gap-4">
+              {location.pathname.startsWith(`/campers/profile/${camperIdFromStorage}/edit`) ? (
+                <Button
+                  onClick={() => navigate(`/campers/profile/${camperIdFromStorage}`)}
+                  size="lg"
+                  className="text-lg bg-transparent hover:bg-[#4c47b4]"
+                >
+                  Ver Perfil
+                </Button>
+              ) : (
+                <Button
+                  onClick={() => navigate(`/campers/profile/${camperIdFromStorage}/edit`)}
+                  size="lg"
+                  className="text-lg bg-transparent hover:bg-[#4c47b4]"
+                >
+                  Editar Perfil
+                </Button>
+              )}
+              <Button size="lg" className="text-lg bg-red-500 hover:bg-red-600" onClick={handleLogout}>
                 Cerrar Sesión
               </Button>
             </div>
@@ -283,3 +234,4 @@ const Navbar = ({ viewType, links, onLinkClick }) => {
 };
 
 export default Navbar;
+
